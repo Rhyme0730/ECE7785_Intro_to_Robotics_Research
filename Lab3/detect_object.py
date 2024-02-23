6d421d31dcac6bed788cddf4c955531ee98a32a9
Rhyme-Rao <124558489+Rhyme0730@users.noreply.github.com>
2024-02-22 19:18:53 -0500

    Update detect_object.py

--- Lab3/detect_object.py
+++ Lab3/detect_object.py
@@ -30,8 +30,9 @@ class detect_object(Node):
         self.publisher2 = self.create_publisher(Point,'/detect_object/coordinates',qos_profile) # publish angles 
 
     def _image_callback(self, CompressedImage):	
-        self.find_object_Hough(CompressedImage)
+        # self.find_object_Hough(CompressedImage)
         # self.find_object_Contour(CompressedImage)
+        self.find_object(CompressedImage)
         
     def find_object_Hough(self, CompressedImage):
         self._imgBGR = CvBridge().compressed_imgmsg_to_cv2(CompressedImage,"bgr8")
@@ -55,35 +56,83 @@ class detect_object(Node):
         self.publisher1.publish(CvBridge().cv2_to_compressed_imgmsg(self._imgBGR))
         self.publisher2.publish(self.msg)
 
-    # def find_object_Contour(self, CompressedImage):
-    #     self._imgBGR = CvBridge().compressed_imgmsg_to_cv2(CompressedImage,"bgr8")
-    #     self.hsv_image = cv2.cvtColor(self._imgBGR, cv2.COLOR_BGR2HSV)
+    def find_object_Contour(self, CompressedImage):
+        self._imgBGR = CvBridge().compressed_imgmsg_to_cv2(CompressedImage,"bgr8")
+        self.hsv_image = cv2.cvtColor(self._imgBGR, cv2.COLOR_BGR2HSV)
+        self.hsv_image = cv2.medianBlur(self.hsv_image,5)
+        lower = np.array([5, 100, 100])
+        upper = np.array([15, 255, 255])
 
-    #     lower_orange = np.array([5, 50, 50])
-    #     upper_orange = np.array([15, 255, 255])
+        mask = cv2.inRange(self.hsv_image, lower, upper)
 
-    #     mask = cv2.inRange(self.hsv_image, lower_orange, upper_orange)
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    #     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        self.msg=Point()
+        if contours:
+            for contour in contours:
+                area = cv2.contourArea(contour)
+                if area > 200:
+                    x, y, w, h = cv2.boundingRect(contour)
 
-    #     if contours:
-    #         for contour in contours:
-    #             x, y, w, h = cv2.boundingRect(contour)
+                    cv2.rectangle(self._imgBGR, (x, y), (x+w, y+h), (255, 255, 0), 2)
 
-    #             cv2.rectangle(self._imgBGR, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    center_x = x + w // 2
+                    center_y = y + h // 2
+                    self.get_logger().info(f'Coordinates{center_x, center_y}')
+                    self.msg.x = float(center_x)
 
-    #             center_x = x + w // 2
-    #             center_y = y + h // 2
-    #             cv2.circle(self._imgBGR, (center_x, center_y), 5, (255, 0, 0), -1)
-    #             self.get_logger().info(f'Coordinates{center_x, center_y}')
+        else:
+            self.get_logger().info('No contours found')
+            self.msg.x = 9999.0
 
-    #     else:
-    #         self.get_logger().info('No contours found')
-    #         self.msg = Point()
-    #         self.msg.x = 9999.0
+        self.publisher1.publish(CvBridge().cv2_to_compressed_imgmsg(self._imgBGR))
+        self.publisher2.publish(self.msg)
+    
+    def find_object(self, CompressedImage):
+        # Transform the compressed image to a color image in BGR space
+        self._imgBGR = CvBridge().compressed_imgmsg_to_cv2(CompressedImage, "bgr8")
+        
+        bgr = self._imgBGR
+        # Convert BGR to HSV
+        hsv = cv2.cvtColor(self._imgBGR, cv2.COLOR_BGR2HSV)
 
-    #     self.publisher1.publish(CvBridge().cv2_to_compressed_imgmsg(self._imgBGR))
-    #     self.publisher2.publish(self.msg)
+
+        # Define orange color range in HSV
+        # lower_orange = np.array([5, 100, 100])
+        # upper_orange = np.array([15, 255, 255])
+   
+        lower_orange = np.array([20, 50, 140])
+        upper_orange = np.array([40, 200, 255])
+
+
+        # Threshold the HSV image to get only orange colors
+        mask = cv2.inRange(hsv, lower_orange, upper_orange)
+
+
+        # Bitwise-AND mask and original image
+        orange_object = cv2.bitwise_and(bgr, bgr, mask=mask)
+
+
+        # Find contours in the orange object
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+        if contours:
+            # Get the largest contour (assuming it's the orange object)
+            max_contour = max(contours, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(max_contour)
+            # Calculate the middle coordinates of the object
+            middle_x = x + w // 2
+            middle_y = y + h // 2
+            cv2.rectangle(self._imgBGR, (x, y), (x+w, y+h), (255, 255, 0), 2)
+            angle = float(((middle_x-160)/320)*62.2)
+        else:
+            angle, middle_y = 9999.0, -1
+
+        msg = Point()
+        msg.x = angle
+        self.publisher1.publish(CvBridge().cv2_to_compressed_imgmsg(self._imgBGR))
+        self.publisher2.publish(msg)
 
 def main():
     rclpy.init()

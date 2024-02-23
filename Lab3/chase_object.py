1af78125450dca81064d804c1004c4fd7744d507
Rhyme-Rao <124558489+Rhyme0730@users.noreply.github.com>
2024-02-22 19:18:38 -0500

    Update chase_object.py

--- Lab3/chase_object.py
+++ Lab3/chase_object.py
@@ -28,13 +28,15 @@ class chase_object(Node):
             qos_profile
         )        
 
-        self.publisher = self.create_publisher(Twist,'/cmd_vel',5)
-        self.angle = 0
+        self.publisher = self.create_publisher(Twist,'/cmd_vel',1)
+        self.angle = 0.0
         self.dist = 0.4
         self.safe_dist = 0.4
+        # self.Kp_v = 1.3
+        # self.Kp_a = 0.15 # this can work!
         self.Kp_v = 1
         self.Kp_a = 0.1
-        self.timer_1 = self.create_timer(0.5, self.angle_position_PID)
+        self.timer_1 = self.create_timer(0.1, self.angle_position_PID)
         
     def get_coordinates(self,msg):
         self.angle = msg.x
@@ -46,19 +48,22 @@ class chase_object(Node):
             cmd.linear.x = 0.0
             cmd.angular.z = 0.0
 
-        elif (np.abs(self.angle) <= 8):
+        elif (np.abs(self.angle) <= 4):
             cmd.angular.z = 0.0     # threshold of tracking area
 
         else:
             cmd.angular.z = self.limit(-self.Kp_a*self.angle, -0.5, 0.5)   # P control of angle 
+            self.get_logger().info(f'control_vel = {cmd.angular.z}')
+
 
         if self.dist <= 3:
             error = self.dist - self.safe_dist
-	    if np.abs(error) <= 0.05: # allow robot to stay in a approximate range 
-	    	cmd.linear.x = 0.0		
-	    else:
-		cmd.linear.x = self.limit(self.Kp_v*error,-0.1, 0.1)
-		    
+            if np.abs(error) <= 0.02: # allow robot to stay in a approximate range 
+                cmd.linear.x = 0.0		
+            else:
+                cmd.linear.x = self.limit(self.Kp_v*error,-0.1, 0.1)
+                self.get_logger().info(f'control_ang = {cmd.linear.x}')
+
         self.publisher.publish(cmd)
         
     # Limit function
